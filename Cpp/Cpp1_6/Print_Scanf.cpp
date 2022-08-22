bb4b89240117ea2445cd8969c29f98ff5f7b463d
Kang-Jae-Wook <82699453+Kang-Jae-Wook@users.noreply.github.com>
2022-08-22 23:34:42 +0900

    prsc
    printf
    scanf

---
+++ Cpp/Cpp1_6/Print_Scanf.cpp
@@ -0,0 +1,77 @@
+#pragma warning(disable: 4996)
+#include <cstdio>
+
+int main()
+{
+	//	printf("%d + %d = %i\n", 2, 3, 2 + 3);
+	//	
+	//	char ch = 'b';
+	//	printf("%c%c\n", 'a', ch);
+	//	
+	//	printf("%f - %f = %1f\n", 0.1f, 0.5f, 0.1f - 0.5f);
+	//	
+	//	printf("%x + %x = %X\n\n", 10, 10, 10); // x = 16진수 10~15 (a~f) X = 10~15 A~F  | o = 8진수
+	//	
+	//	printf("%-10da\n", 1);
+	//	printf("%-10da\n", -1);
+	//	printf("%-10fa\n", 1.1);
+	//	printf("%-10fa\n", -1.1);
+	//	
+	//	printf("%%\n");
+
+	//	freopen("output.txt", "w", stdout);
+	//	printf("Hello World!!!!!!!!!!!!!!!!!!!!!!!!\n");   
+
+	// printf = 파일 콘솔에 적용됨 
+
+	//	int num = 0;
+	//	char ch;
+	//	int n = scanf("%d %c", &num, &ch);
+	//	
+	//	if (scanf("%d", &num) != 1)
+	//	printf("%d", num);
+
+	//	int num0, num1, num2;
+	//	scanf("%d + %d = %d", &num0, &num1, &num2);
+	//	printf("%d, %d, %d", num0, num1, num2);
+
+	//int num0 = 0;
+	//	char ch;
+	//	scanf("%c", &ch);
+	//	printf("%c", ch);
+	//	printf("%i", ch);
+
+	//	float f0;
+	//	double d0, d1;
+	//	scanf("%lf %lf %f", &d0, &d1, &f0);
+	//	printf("%lf %lf %f", d0, d1, f0);
+	//	scanf("%lf", &f0);
+	//	printf("%lf", f0);
+
+	//	int num;
+	//	scanf("%o", &num);
+	//	scanf("%x", &num);
+	//	printf("%d", num);
+
+	//	char ch;
+	//	scanf("%c", &ch);
+	//	printf("1, %c\n", ch);
+
+	//	fflush(stdin);
+	//	ch = getchar();
+	//	printf("%d", ch);
+
+	//	while (getchar() != '\n');
+
+	//	scanf("%c", &ch);
+	//	printf("2, %d\n", ch);
+
+	freopen("input.txt", "r", stdin);
+
+	int num;
+	scanf("%d", &num);
+	printf("%d", num);
+
+
+
+}
---
+++ Cpp/Cpp1_6/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <limits>
+#include <cstdio>
+
+
+using namespace std;
+int  main()
+{
+	//	char ch0 = 'a';
+	//	char ch1 = 97;
+	//	
+	//	cout << "ch0 : " << ch0 << endl;
+	//	cout << "ch1 : " << ch1 << endl;
+	//	
+	//	cout << "ch0 : " << (int)ch0 << endl;
+	//	cout << "ch1 : " << int(ch0) << endl;
+	//	
+	//	
+	//	cout << sizeof(ch0) << endl;
+	//	cout << sizeof(ch1) << endl;
+
+	//	cout << (int)numeric_limits<uint8_t>::max() << endl;
+
+	//	bool b0 = 0 == 0;
+	//	bool b1 = 0 < 1;
+	//	bool b2 = 0 > 1;
+	//	
+	//	cout.setf(ios_base::boolalpha);
+	//	cout << "b0 : " << b0 << endl;
+	//	cout << "b1 : " << b1 << endl;
+	//	cout << "b2 : " << b2 << endl;
+	//	
+	//	if (b0)
+	//		cout << "b0" << endl;
+	//	if (b1)
+	//		cout << "b1" << endl;
+	//	if (b2)
+	//		cout << "b2" << endl;
+
+	//	int itrue = true;
+	//	int ifalse = false;
+	//	
+	//	cout << itrue << endl;
+	//	cout << ifalse << endl;
+
+	//	bool btrue = 100;
+	//	bool bfalse = 0;
+	//	
+	//	cout << btrue << endl;
+	//	cout << bfalse << endl;
+
+	printf("%d + %d = %i", 2, 3, 2+3);
+
+
+
+}
